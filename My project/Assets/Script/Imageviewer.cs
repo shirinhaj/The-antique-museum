7b12df3e865a3c54a800b46b39fe8b3f24559000
Shirin Hajahmadi <62059613+shirinhaj@users.noreply.github.com>
2022-06-30 14:50:09 +0200

    ThirdPush

---
+++ My project/Assets/Script/Imageviewer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Imageviewer : MonoBehaviour
+{
+    public int numOFCurrentImage;
+
+    public List<Material> materials = new List<Material>();
+
+    public GameObject screen;
+
+    public void PreviousFunc()
+    {
+        if(numOFCurrentImage >= 1)
+        {
+            numOFCurrentImage--;
+            screen.GetComponent<Renderer>().material = materials[numOFCurrentImage];
+        }
+    }
+
+    public void NextFunc()
+    {
+        if(numOFCurrentImage < materials.Count -1)
+        {
+            numOFCurrentImage++;
+            screen.GetComponent<Renderer>().material = materials[numOFCurrentImage];
+        }
+    }
+
+}
