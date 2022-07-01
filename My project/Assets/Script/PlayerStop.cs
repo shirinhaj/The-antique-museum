656c0cdbdb635bb12ccaa13652602a55f48325ef
Shirin Hajahmadi <62059613+shirinhaj@users.noreply.github.com>
2022-07-01 22:39:19 +0200

    Next

---
+++ My project/Assets/Script/PlayerStop.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStop : MonoBehaviour
+{
+    public MoveForward playerMovement;
+    void OnCollisionEnter(Collision collision)
+    {
+        playerMovement.enabled = false;
+    }
+}
